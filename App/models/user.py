55904b15246458ddd0e346476e3f0623bad7b292
devern-DZZC <devern.chattergoon@my.uwi.edu>
2025-04-15 23:24:29 -0400

    fixed login issue

--- App/models/user.py
+++ App/models/user.py
@@ -1,8 +1,7 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
-from flask_login import UserMixin
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
